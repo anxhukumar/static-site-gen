f5671e859214e6022e6cf403e48da2176a0a3e81
anxhukumar <contact.anshukumar@protonmail.com>
2025-06-24 12:57:33 +0530

    Make changes in main.py to use ./docs

--- src/main.py
+++ src/main.py
@@ -4,17 +4,16 @@ import shutil
 import sys
 import os
 
-
-# delete all the contents of public directory
-if os.path.exists("./public"):
-        shutil.rmtree("./public")
-
-# copying all the content of static and pasting to public
-static_to_public("./static", "./public")
-
 base_path = sys.argv[1]
 if not base_path:
         base_path = "/"
+
+# delete all the contents of public directory
+if os.path.exists("./docs"):
+        shutil.rmtree("./docs")
+
+# copying all the content of static and pasting to public
+static_to_public("./static", "./docs")
 
 # generate page
 generate_pages_recursive("./content", "./template.html", "./docs", base_path)
